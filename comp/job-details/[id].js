ccf82542bf58f30dc43694dc6a1f31adda110d89
Poojan <pmp15091995@gmail.com>
2023-05-01 04:18:12 +0530

    Started with JobDetails page

---
+++ comp/job-details/[id].js
@@ -0,0 +1,15 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useState, useCallback } from "react";
+const JobDetails = () => {
+  return <Text>JobDetails</Text>;
+};
+
+export default JobDetails;
